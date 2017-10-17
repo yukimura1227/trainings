8d631f33d9839ee93d0978541490bd52d5acb3e4
yukimura1227 <takamura1227@gmail.com>
2017-10-17 14:57:04 +0900

    add color settings to ggplot2 sample

--- sample/ggplot2/sample.r
+++ sample/ggplot2/sample.r
@@ -1,10 +1,11 @@
 library(ggplot2)
 theme_set(theme_gray(base_family ='Migu 1M'))
 
-
+# 散布図(sample of qplot)
 qplot(
   carat,
   price,
+  colour = cut,
   data = diamonds,
   main ='ダイヤモンドのカラットと価格の関係',
   xlab='カラット',
