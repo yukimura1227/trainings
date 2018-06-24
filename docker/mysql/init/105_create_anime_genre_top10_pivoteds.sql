29caba130f096575350c73739536ad02b128b058
yukimura1227 <takamura1227@gmail.com>
2018-06-24 15:07:08 +0900

    fix wrong type

--- docker/mysql/init/105_create_anime_genre_top10_pivoteds.sql
+++ docker/mysql/init/105_create_anime_genre_top10_pivoteds.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS `jupyter`.`anime_genre_top10_pivoteds` (
   `genre` varchar(255) NOT NULL DEFAULT '',
-  `Movie` varchar(255) NOT NULL DEFAULT '',
-  `Music` varchar(255) NOT NULL DEFAULT '',
-  `ONA` varchar(255) NOT NULL DEFAULT '',
-  `OVA` varchar(255) NOT NULL DEFAULT '',
-  `Special` varchar(255) NOT NULL DEFAULT '',
-  `TV` varchar(255) NOT NULL DEFAULT '',
+  `Movie` float NOT NULL,
+  `Music` float NOT NULL,
+  `ONA` float NOT NULL,
+  `OVA` float NOT NULL,
+  `Special` float NOT NULL,
+  `TV` float NOT NULL,
   INDEX (`genre`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
