11f994bc977cbcd9568309d6daef8d58c1722883
yukimura1227 <takamura1227@gmail.com>
2018-07-17 17:05:27 +0900

    take fullPage screen shot

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+  page.setViewport({width: 1200, height: 800})
+
   await page.goto('http://yukimura1227.blog.fc2.com/');
-  await page.screenshot({path: 'test_result/example.png'});
+  await page.waitForNavigation({waitUntil:'networkidle2', timeout:5000})
+            .catch(e => console.log('timeout exceed. proceed to next operation'))
+  await page.screenshot({path: 'test_result/example.png', fullPage: true});
 
   await browser.close();
 })();
