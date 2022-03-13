28d55f3c343e58d9ac5b7963cc431ca552453e3d
yukimura1227 <takamura1227@gmail.com>
2022-03-13 17:13:41 +0900

    chore: cargoの使い方を追記

--- rust_training/projects/hello_cargo/src/main.rs
+++ rust_training/projects/hello_cargo/src/main.rs
@@ -1,3 +1,7 @@
+// https://doc.rust-jp.rs/book-ja/ch01-03-hello-cargo.html
+// cargo build # proejctのコンパイルして、実行ファイルを生成
+// cargo run # 実行ファイルを実行
+// cargo check # projectのコンパイルチェックだけ(実行ファイルを作らない分高速)
 fn main() {
     println!("Hello, world!");
 }
