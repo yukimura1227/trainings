873132e7d1eee607b15af4959b1dcfb3b4473db7
yukimura1227 <takamura1227@gmail.com>
2022-03-13 17:22:44 +0900

    chore: https://doc.rust-jp.rs/book-ja/ch03-01-variables-and-mutability.html

--- rust_training/projects/variables/src/main.rs
+++ rust_training/projects/variables/src/main.rs
@@ -1,3 +1,12 @@
+// https://doc.rust-jp.rs/book-ja/ch03-01-variables-and-mutability.html
 fn main() {
-    println!("Hello, world!");
+    let x = 5;
+    println!("The Value Of x is {}", x);
+    // 以下はcompileエラーになる。letで宣言したものを変更しようとしているから
+    // x = 6;
+    // println!("The Value Of x is {}", x);
+    let mut y = 5;
+    println!("The Value Of y is {}", y);
+    y = 6;
+    println!("The Value Of y is {}", y);
 }
