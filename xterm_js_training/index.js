606c4c448000792204f3b39e4047179d2b0704d0
yukimura1227 <takamura1227@gmail.com>
2023-03-05 11:08:58 +0900

    feat: node-ptyでサーバ側でptyを立ち上げて、WebSocket経由でコマンドを実行できるように実装

--- xterm_js_training/index.js
+++ xterm_js_training/index.js
@@ -1,13 +1,28 @@
 import { WebSocketServer } from 'ws';
+import * as nodePty from 'node-pty';
 
 const wss = new WebSocketServer({ port: 8080 });
 
 wss.on('connection', function connection(ws) {
+  const pty = nodePty.spawn("bash", ["--login"], {
+    name: "xterm-color",
+    cols: 80,
+    rows: 24,
+    cwd: process.env.HOME,
+  });
+  pty.onData((data) => {
+    console.log("send: %s", data);
+    ws.send(JSON.stringify({ output: data }));
+  });
   ws.on('error', console.error);
 
-  ws.on('message', function message(data) {
-    console.log('received: %s', data);
-  });
+  ws.on('message', (message) => {
+    console.log('received: %s', message);
 
-  ws.send('something');
+    const m = JSON.parse(message.toString());
+
+    if (m.input) {
+      pty.write(m.input);
+    }
+  });
 });
