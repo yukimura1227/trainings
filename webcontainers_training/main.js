9f393c74905d851a45bba9a5b25248bf77bce736
yukimura1227 <takamura1227@gmail.com>
2023-03-25 06:38:23 +0900

    chore: tutorial step 2 and 3 [https://webcontainers.io/tutorial/2-setting-up-webcontainers https://webcontainers.io/tutorial/3-installing-dependencies]

--- webcontainers_training/main.js
+++ webcontainers_training/main.js
@@ -1,10 +1,9 @@
 import './style.css';
+import { WebContainer } from '@webcontainer/api';
+import { files } from './files';
 
-/** @type {HTMLIFrameElement | null} */
-const iframeEl = document.querySelector('iframe');
-
-/** @type {HTMLTextAreaElement | null} */
-const textareaEl = document.querySelector('textarea');
+/** @type {import('@webcontainer/api').WebContainer}  */
+let webcontainerInstance;
 
 document.querySelector('#app').innerHTML = `
   <div class="container">
@@ -16,3 +15,42 @@ document.querySelector('#app').innerHTML = `
     </div>
   </div>
 `
+/** @type {HTMLIFrameElement | null} */
+const iframeEl = document.querySelector('iframe');
+
+/** @type {HTMLTextAreaElement | null} */
+const textareaEl = document.querySelector('textarea');
+
+window.addEventListener('load', async () => {
+ // Call only once
+  webcontainerInstance = await WebContainer.boot();
+  await webcontainerInstance.mount(files);
+
+  const installResult = await installDependencies();
+  console.log(installResult);
+  console.log(installResult.output);
+
+  textareaEl.value = installResult.output;
+});
+
+// sample for command output for exit code
+// async function installDependencies() {
+//   // Install dependencies
+//   const installProcess = await webcontainerInstance.spawn('npm', ['install']);
+//   // Wait for install command to exit
+//   return { exitCode: installProcess.exit, outputStream: installProcess.output };
+// }
+
+// sample for command output for stream instead of exit code
+const installDependencies = async () => {
+  const installProcess = await webcontainerInstance.spawn('npm', ['install']);
+  const output = "";
+  installProcess.output.pipeTo(new WritableStream({
+    write(data) {
+      output.concat(data);
+      console.log(data);
+    }
+  }));
+
+  return { exitCode: installProcess.exit, output: output }
+}
