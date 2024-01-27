112124a1fa4edd68c46ec901aa36a6ca5bb7ea94
yukimura1227 <takamura1227@gmail.com>
2024-01-27 12:39:04 +0900

    chore: section 1.4.1

--- rust_training_book/project/1/hello2/tests/cli.rs
+++ rust_training_book/project/1/hello2/tests/cli.rs
@@ -1,6 +1,6 @@
 // cargo test
 
-use std::process::Command;
+use assert_cmd::Command;
 
 # [test]
 // fn works() {
@@ -9,7 +9,6 @@ use std::process::Command;
 // }
 
 fn runs() {
-  let mut cmd = Command::new("ls");
-  let res = cmd.output();
-  assert!(res.is_ok());
+  let mut cmd = Command::cargo_bin("hello2").unwrap();
+  cmd.assert().success();
 }
