07a4f7c04bd42344ab3556f010d9b780d78dd90d
yukimura1227 <takamura1227@gmail.com>
2024-02-03 11:59:43 +0900

    chore: section 2.2.3

--- rust_training_book/project/2/recho/src/main.rs
+++ rust_training_book/project/2/recho/src/main.rs
@@ -1,3 +1,24 @@
+use clap::{App, Arg};
+
 fn main() {
-    println!("{:?}", std::env::args());
+  let matches = App::new("recho")
+    .version("0.1.0")
+    .author("yukimura1227")
+    .about("echo command implemented by Rust")
+    .arg(
+      Arg::with_name("text")
+        .value_name("TEXT")
+        .help("input text")
+        .required(true)
+        .min_values(1),
+    )
+    .arg(
+      Arg::with_name("omit_newline")
+        .short("n")
+        .help("Do not print newline")
+        .takes_value(false),
+    )
+    .get_matches();
+
+  println!("{:#?}", matches);
 }
